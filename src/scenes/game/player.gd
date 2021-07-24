af369f3aecd6060d195866a6c4c6e6079bccd7f4
http://jneen.net/ <jneen@jneen.net>
2021-07-24 11:47:48 -0400

    add crown controls

--- src/scenes/game/player.gd
+++ src/scenes/game/player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 export (int) var walk_speed = 1000
 export (int) var run_speed = 1600
+export (int) var carry_speed = 700
 export (int) var jump_speed = -1800
 export (int) var gravity_float = 3500
 export (int) var gravity_fall = 5000
@@ -12,17 +13,19 @@ var velocity = Vector2.ZERO
 export (float, 0, 1.0) var friction = 0.1
 export (float, 0, 1.0) var acceleration = 0.25
 
-var wearing_crown : bool
 var coins : int = 0
 var invicible : bool = false
-var jump_peaked = false
+var is_wearing_crown : bool
+var is_carrying : bool = false
+var is_ducking : bool = false
+var facing_dir : int = 0
 
 func damage(value : float):
 	# skip if we're invincible
-	if not $InvicibleTimer.is_stopped(): return
+	if invicible: return
 
-	if wearing_crown:
-		wearing_crown = false
+	if is_wearing_crown:
+		lose_crown()
 		$InvicibleTimer.start()
 	else:
 		splat()
@@ -31,11 +34,22 @@ func splat():
 	print('TODO: splat')
 
 func get_input():
+	var moving = false
 	var dir = 0
+
 	if Input.is_action_pressed("walk_right"):
 		dir += 1
+		facing_dir = 1
 	if Input.is_action_pressed("walk_left"):
-		dir -= 1
+		dir += -1
+		facing_dir = -1
+
+	if is_wearing_crown:
+		move_player(facing_dir)
+	else:
+		move_player(dir)
+
+func move_player(dir):
 	if dir != 0:
 		velocity.x = lerp(velocity.x, dir * pick_speed(), acceleration)
 	else:
@@ -48,10 +62,18 @@ func pick_gravity():
 		return gravity_fall
 
 func pick_speed():
-	if Input.is_action_pressed("interact"):
-		return run_speed
-	else:
-		return walk_speed
+	if is_wearing_crown: return run_speed
+	if Input.is_action_pressed("interact"): return run_speed
+	if is_carrying: return carry_speed
+	return walk_speed
+
+func wear_crown():
+	# TODO: animation + sfx
+	is_wearing_crown = true
+
+func lose_crown():
+	# TODO: animation + sfx
+	is_wearing_crown = false
 
 func _physics_process(delta):
 	get_input()
@@ -61,6 +83,11 @@ func _physics_process(delta):
 		if is_on_floor():
 			velocity.y = jump_speed
 			velocity.x *= jump_boost
+
+	# [jneen] TODO: if is_carrying_crown():
+	if Input.is_action_just_pressed("duck"):
+		wear_crown()
+
 	Hud.get_node("UI/Coins/CoinsLabel").text = str(coins)
 	Hud.get_node("UI/Heart/Label").text = str(coins) + "%"
 
